eae67972e6e96334ea9beab6f9b5d4f31c133f22
Ulf Benjaminsson <ulf.benjaminsson@speldesign.uu.se>
2023-12-12 16:08:15 +0100

    Fixed formatting and removed comments from game.h

--- Source/game.h
+++ Source/game.h
@@ -4,203 +4,133 @@
 #include "Resources.h"
 #include <string>
 
-
-enum struct State
-{
-	STARTSCREEN,
-	GAMEPLAY,
-	ENDSCREEN
+enum struct State{
+    STARTSCREEN,
+    GAMEPLAY,
+    ENDSCREEN
 };
 
-enum struct EntityType
-{
-	PLAYER,
-	ENEMY,
-	PLAYER_PROJECTILE,
-	ENEMY_PROJECTILE
+enum struct EntityType{
+    PLAYER,
+    ENEMY,
+    PLAYER_PROJECTILE,
+    ENEMY_PROJECTILE
 };
 
-struct PlayerData
-{
-	std::string name;
-	int score;
+struct PlayerData{
+    std::string name;
+    int score;
 };
 
-struct Player
-{
-public:
-
-	float x_pos = 0;
-	float speed = 7;
-	float player_base_height = 70.0f;  
-	float radius = 50;
-	int lives = 3;
-	int direction = 0;
-	int activeTexture = 0;
-	float timer = 0;
-
-	EntityType type = EntityType::PLAYER;
-
-	void Initialize();
-	void Render(Texture2D texture);
-	void Update();
-	
+struct Player{
+    float x_pos = 0;
+    float speed = 7;
+    float player_base_height = 70.0f;
+    float radius = 50;
+    int lives = 3;
+    int direction = 0;
+    int activeTexture = 0;
+    float timer = 0;
+    EntityType type = EntityType::PLAYER;
+    void Initialize();
+    void Render(Texture2D texture);
+    void Update();
 };
 
 
-struct Projectile
-{
-public: 
-	// INITIALIZE PROJECTILE WHILE DEFINING IF ITS PLAYER OR ENEMY 
-	Vector2 position = {0,0};
-	int speed = 15; 
-	bool active = true;  
-	EntityType type = {};
-
-	// LINE WILL UPDATE WITH POSITION FOR CALCULATIONS
-	Vector2 lineStart = { 0, 0 };
-	Vector2 lineEnd = { 0, 0 };
-
-	void Update();
-
-	void Render(Texture2D texture);
+struct Projectile{
+    Vector2 position = {0,0};
+    int speed = 15;
+    bool active = true;
+    EntityType type = {};
+    Vector2 lineStart = {0, 0};
+    Vector2 lineEnd = {0, 0};
+    void Update();
+    void Render(Texture2D texture);
 };
 
-struct Wall 
-{
-public: 
-	Vector2 position; 
-	Rectangle rec; 
-	bool active; 
-	Color color; 
-	int health = 50;
-	int radius = 60;
-
-
-	void Render(Texture2D texture); 
-	void Update(); 
+struct Wall{
+    Vector2 position;
+    Rectangle rec;
+    bool active;
+    Color color;
+    int health = 50;
+    int radius = 60;
+    void Render(Texture2D texture);
+    void Update();
 };
 
-struct Alien
-{
-public:
-	
-	Color color = WHITE; 
-	Vector2 position = {0, 0};
-	int x = 0; 
-	int y = 0; 
-	float radius = 30;
-	bool active = true;  
-	bool moveRight = true; 
-	
-	EntityType type = EntityType::ENEMY; 
-
-	int speed = 2; 
-		 
-	void Update(); 
-	void Render(Texture2D texture); 
+struct Alien{
+    EntityType type = EntityType::ENEMY;
+    Color color = WHITE;
+    Vector2 position = {0, 0};
+    int x = 0;
+    int y = 0;
+    float radius = 30;
+    bool active = true;
+    bool moveRight = true;
+    int speed = 2;
+    void Update();
+    void Render(Texture2D texture);
 };
 
 
-struct Star
-{
-	Vector2 initPosition = { 0, 0 };
-	Vector2 position = { 0, 0 };
-	Color color = GRAY;
-	float size = 0;
-	void Update(float starOffset);
-	void Render();
+struct Star{
+    Vector2 initPosition = {0, 0};
+    Vector2 position = {0, 0};
+    Color color = GRAY;
+    float size = 0;
+    void Update(float starOffset);
+    void Render();
 };
 
-struct Background
-{
-	
-
-	std::vector<Star> Stars;
-
-	void Initialize(int starAmount);
-	void Update(float offset);
-	void Render();
-
+struct Background{
+    std::vector<Star> Stars;
+    void Initialize(int starAmount);
+    void Update(float offset);
+    void Render();
 };
 
-struct Game
-{
-	// Gamestate
-	State gameState = State::STARTSCREEN;
-
-	// Score
-	int score;
-
-	// for later, make a file where you can adjust the number of walls (config file) 
-	int wallCount = 5;
-
-	//Aliens shooting
-	float shootTimer = 0;
-
-	//Aliens stuff? (idk cause liv wrote this)
-	Rectangle rec = { 0, 0 ,0 ,0 }; 
-
-	int formationWidth = 8;
-	int formationHeight = 5;
-	int alienSpacing = 80;
-	int formationX = 100;
-	int formationY = 50;
-
-	bool newHighScore = false;
-	
-
-	void Start();
-	void End();
-
-	void Continue();	
-
-	void Update();
-	void Render();
-
-	void SpawnAliens();
-
-	bool CheckCollision(Vector2 circlePos, float circleRadius, Vector2 lineTop, Vector2 lineBottom);
-
-	bool CheckNewHighScore();
-
-	void InsertNewHighScore(std::string name);
-
-	void LoadLeaderboard();
-	void SaveLeaderboard();
+struct Game{    
+    State gameState = State::STARTSCREEN;
+    Resources resources{};
+    Player player;
+    Background background;
+    std::vector<Projectile> Projectiles;
+    std::vector<Wall> Walls;
+    std::vector<Alien> Aliens;
+    std::vector<PlayerData> Leaderboard = {{"Player 1", 500}, {"Player 2", 400}, {"Player 3", 300}, {"Player 4", 200}, {"Player 5", 100}};
+    
+    Vector2 playerPos;
+    Vector2 alienPos;
+    Vector2 cornerPos;
+    float offset;    
+    char name[9 + 1] = "\0";
+    int letterCount = 0;
+    Rectangle textBox = {600, 500, 225, 50};
+    bool mouseOnText = false;
+    int framesCounter = 0;
+    int score;
+    int wallCount = 5;
+    float shootTimer = 0;
+    Rectangle rec = {0, 0 ,0 ,0};
+    int formationWidth = 8;
+    int formationHeight = 5;
+    int alienSpacing = 80;
+    int formationX = 100;
+    int formationY = 50;
+    bool newHighScore = false;
 
 
-	// Entity Storage and Resources
-	Resources resources{};
-
-	Player player;
-
-	std::vector<Projectile> Projectiles;
-
-	std::vector<Wall> Walls;
-
-	std::vector<Alien> Aliens;
-
-	std::vector<PlayerData> Leaderboard = { {"Player 1", 500}, {"Player 2", 400}, {"Player 3", 300}, {"Player 4", 200}, {"Player 5", 100} };
-	
-	Background background;
-
-
-
-	Vector2 playerPos;
-	Vector2 alienPos; 
-	Vector2 cornerPos;
-	float offset;
-
-
-
-	//TEXTBOX ENTER
-	char name[9 + 1] = "\0";      //One extra space required for null terminator char '\0'
-	int letterCount = 0;
-
-	Rectangle textBox = { 600, 500, 225, 50 };
-	bool mouseOnText = false;
-
-	int framesCounter = 0;
-
+    void Start();
+    void End();
+    void Continue();
+    void Update();
+    void Render();
+    void SpawnAliens();
+    bool CheckCollision(Vector2 circlePos, float circleRadius, Vector2 lineTop, Vector2 lineBottom);
+    bool CheckNewHighScore();
+    void InsertNewHighScore(std::string name);
+    void LoadLeaderboard();
+    void SaveLeaderboard();
 };
