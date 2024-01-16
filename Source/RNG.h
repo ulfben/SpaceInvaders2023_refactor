dfbae9a63256adaa5d2b6e56d9d370e593fb4832
Ulf Benjaminsson <ulf.benjaminsson@speldesign.uu.se>
2024-01-16 16:15:00 +0100

    Adding jump() to the random number generator Improving createSeeds() Adding type aliases to the rng public interface

--- Source/RNG.h
+++ Source/RNG.h
@@ -1,14 +1,16 @@
 #pragma once
-#include <algorithm>
 #include <array>
 #include <cassert>
+#include <bitset>
 #include <chrono> //for entropy in createSeeds
 #include <concepts>
-#include <ctime> //for entropy in createSeeds
+#include <cstdint>
 #include <limits>
 #include <span>
 #include <thread> //for entropy in createSeeds
+#include <ctime> //for entropy in createSeeds
 #include <type_traits>
+
 // The "xoshiro256** 1.0" generator.
 // public interface, rejection sampling and seeding utilities by Ulf Benjaminsson (2023)
 // https://ulfbenjaminsson.com/
@@ -16,12 +18,13 @@
 // https://quuxplusone.github.io/blog/2021/11/23/xoshiro/
 // of the C version by David Blackman and Sebastiano Vigna (2018),
 // https://prng.di.unimi.it/xoshiro256starstar.c
-static_assert(sizeof(long long) == 8, "64-bit machines only");
 
 class RNG{
 public:
-    using u64 = unsigned long long;
+    using u64 = std::uint_fast64_t;
     static constexpr std::size_t SEED_COUNT = 4;
+    using State = std::array<u64, SEED_COUNT>;
+    using Span = std::span<const u64, SEED_COUNT>;
     static constexpr auto USE_REJECTION_SAMPLING = false;
     // When enabled, all ranged functions will use rejection sampling to ensure a more uniform 
     // distribution of random numbers across large ranges. The concern with large ranges is that
@@ -31,7 +34,6 @@ public:
     // rejection sampling to ensure uniformity.
 
     constexpr explicit RNG(u64 seed) noexcept{
-        [[gsl::suppress(bounds.4)]]
         s[0] = splitmix64(seed);
         seed += 0x9E3779B97F4A7C15uLL;
         s[1] = splitmix64(seed);
@@ -43,7 +45,7 @@ public:
     constexpr explicit RNG(double seed) noexcept
         : RNG(static_cast<u64>(seed)){}
 
-    constexpr explicit RNG(std::span<const u64, SEED_COUNT> seeds) noexcept{
+    constexpr explicit RNG(Span seeds) noexcept{
         std::ranges::copy(seeds, s.begin());
     }
 
@@ -97,19 +99,45 @@ public:
         }
         if constexpr(USE_REJECTION_SAMPLING){
             return uniformRandom(range);
-        } 
+        }
         return next() / (max() / range);
     }
 
-    constexpr std::span<const u64, SEED_COUNT> state() const{
+    constexpr Span state() const{
         return {s};
     }
 
+    /* the jump() function is equivalent to 2^128 calls to next();
+    it can be used to generate 2^128 non-overlapping subsequences
+    for parallel computations. */
+    constexpr void jump() noexcept{
+        constexpr std::array<std::bitset<64>, SEED_COUNT> JUMP{
+            0x180ec6d33cfd0abaULL, 0xd5a61266f0c9392cULL, 0xa9582618e03fc9aaULL, 0x39abdc4529b1661cULL
+        };
+        State temp{0};
+        for(const auto& bits : JUMP){
+            for(std::size_t b = 0; b < 64; ++b){
+                if(bits.test(b)){
+                    temp[0] ^= s[0];
+                    temp[1] ^= s[1];
+                    temp[2] ^= s[2];
+                    temp[3] ^= s[3];
+                }
+                next();
+            }
+        }
+        s = temp;
+    }
+
+    //convenience function to allow hashing from const values.
+    static constexpr u64 splitmix64_hash(u64 x) noexcept{        
+        return splitmix64(x);
+    }
+    
 private:
-    std::array<u64, SEED_COUNT> s{};
+    State s{};
 
     constexpr u64 nextU64() noexcept{
-        [[gsl::suppress(bounds.4)]]
         const u64 result = rotl(s[1] * 5, 7) * 9;
         const u64 t = s[1] << 17;
         s[2] ^= s[0];
@@ -149,25 +177,28 @@ private:
 
 //Some strategies for seeding the full 256-bit state of xoshiro256.
 // createSeeds uses date, time-since-launch, CPU time, thread ID, and a memory address as sources of entropy.
-static std::array<RNG::u64, RNG::SEED_COUNT> createSeeds() noexcept {
+// each value is hashed using splitmix64. 
+static typename RNG::State createSeeds() noexcept{
     using u64 = RNG::u64;
     using namespace std::chrono;
-    const auto current_date = system_clock::now().time_since_epoch().count();
-    const auto cpu_time = static_cast<u64>(std::clock());
-    const auto uptime = high_resolution_clock::now().time_since_epoch().count();
-    const auto mixed_time = (static_cast<u64>(uptime) << 1) ^ static_cast<u64>(current_date);
-    int local{};
+    const auto current_date = static_cast<u64>(system_clock::now().time_since_epoch().count());
+    const auto uptime = static_cast<u64>(high_resolution_clock::now().time_since_epoch().count());
+    const auto cpu_time = static_cast<u64>(std::clock());    
+    const auto mixed_time = static_cast<u64>((uptime << 1) ^ current_date);
+    const auto thread_id = static_cast<u64>(std::hash<std::thread::id>{}(std::this_thread::get_id()));    
+    const int local{};
+    const auto local_addr = static_cast<u64>(reinterpret_cast<std::uintptr_t>(&local));
     return {
-        std::hash<u64>()(mixed_time),
-        std::hash<std::thread::id>()(std::this_thread::get_id()),
-        std::hash<u64>()(cpu_time),
-        std::hash<std::uintptr_t>()(reinterpret_cast<std::uintptr_t>(&local)),
+        RNG::splitmix64_hash(mixed_time),
+        RNG::splitmix64_hash(thread_id),
+        RNG::splitmix64_hash(cpu_time),
+        RNG::splitmix64_hash(local_addr),
     };
 }
 
 /* createSeedsB *additionally* mix those sources with entropy from std::random_device. This is likely significantly slow(er).
 #include <random>
-static std::array<RNG::u64, RNG::SEED_COUNT> createSeedsB() {
+static typename RNG::State createSeedsB() {
     using u64 = RNG::u64;
     std::random_device rd;
     auto seeds = createSeeds();
