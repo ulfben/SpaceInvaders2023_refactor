f00cc4050b11f99f9e6e3f088f807b5114efc966
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2026-01-18 18:50:30 +0100

    Let Window RAII wrapper verify the invariant by calling IsWindowReady()

--- Source/Window.h
+++ Source/Window.h
@@ -1,7 +1,12 @@
-#include "UtilsRaylib.h"
+#include <raylib.h>
+#include <stdexcept>
+#include <string_view>
 struct Window{
-    Window(std::string_view title, unsigned width, unsigned height, unsigned fps = 60) noexcept {
+    Window(std::string_view title, unsigned width, unsigned height, unsigned fps = 60){
         InitWindow(width, height, title.data());
+        if(!IsWindowReady()){
+            throw std::runtime_error("Unable to open Raylib window. Check config?");
+        }
         SetTargetFPS(fps);
     }
     ~Window() noexcept {
