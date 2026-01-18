878970af8880fe2675ca3dc111d92f77a18a579a
Ulf Benjaminsson <ulfben@users.noreply.github.com>
2026-01-18 18:53:56 +0100

    Prevented passing string_view.data() to C-APIs requiring null-termination.

--- Source/Window.h
+++ Source/Window.h
@@ -1,9 +1,11 @@
 #include <raylib.h>
 #include <stdexcept>
 #include <string_view>
+#include <string>
 struct Window{
     Window(std::string_view title, unsigned width, unsigned height, unsigned fps = 60){
-        InitWindow(width, height, title.data());
+        const std::string tmp(title);
+        InitWindow(width, height, tmp.c_str());
         if(!IsWindowReady()){
             throw std::runtime_error("Unable to open Raylib window. Check config?");
         }
